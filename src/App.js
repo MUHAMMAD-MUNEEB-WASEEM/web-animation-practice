7f20decb56c42fe08c39fb92ec4c95d13ea0c31c
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2020-11-10 13:28:20 +0500

    Practiced web animation api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react';
+import useWebAnimations, {bounce} from '@wellyshen/use-web-animations';
 
 function App() {
+  const {ref, playState} = useWebAnimations({...bounce});
+
+ /* const {ref, playState, getAnimation} = useWebAnimations({
+    keyframes: [
+      {transform: "translateX(0px)", background: "red"},
+      {transform: "translateX(500px)", background: "yellow"}
+    ],
+    timing: {
+      duration: 3000,
+      iterations: Infinity,
+      direction: "alternate",
+      easing: "ease-in-out",
+    }, 
+    onUpdate: ({playState, animate, animation}) => {
+      console.log("Play State ", playState);
+      console.log("animate ", animate);
+      console.log("animation ", animation);
+    }
+//we can view the result of onUpdate on console
+
+  });
+  */   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div ref={ref} style={{background: "red", width: "100px", height: "50px"}}>
+      Hello World {playState}
+      </div>
     </div>
   );
 }
